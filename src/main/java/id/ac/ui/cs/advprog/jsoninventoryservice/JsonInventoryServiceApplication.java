93ed3458ab3e474c34698df086d6a41e4a3f8f55
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-02-19 17:10:28 +0700

    initial commit

---
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/JsonInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.jsoninventoryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JsonInventoryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JsonInventoryServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/id/ac/ui/cs/advprog/jsoninventoryservice/JsonInventoryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package id.ac.ui.cs.advprog.jsoninventoryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JsonInventoryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
