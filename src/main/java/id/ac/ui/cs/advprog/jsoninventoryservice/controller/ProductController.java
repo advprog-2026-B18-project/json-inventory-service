a363d832e2a00ddcc4e9e418a6eac57107482014
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-02-20 13:57:43 +0700

    feat: implement inventory controller, model, and repository

---
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/controller/ProductController.java
@@ -0,0 +1,25 @@
+package id.ac.ui.cs.advprog.jsoninventoryservice.controller;
+
+import id.ac.ui.cs.advprog.jsoninventoryservice.model.Product;
+import id.ac.ui.cs.advprog.jsoninventoryservice.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@CrossOrigin(origins = "*")
+public class ProductController {
+    @Autowired
+    private ProductRepository productRepository;
+
+    @GetMapping
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+    @PostMapping
+    public Product create(@RequestBody Product product) {
+        return productRepository.save(product);
+    }
+}
