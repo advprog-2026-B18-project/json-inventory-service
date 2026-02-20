1d21f6983631965410ce17c99fdbf86d76317265
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-02-20 14:35:24 +0700

    fix: resolve sonarcloud security hotspot

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/controller/ProductController.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/controller/ProductController.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/products")
-@CrossOrigin(origins = "*")
+@CrossOrigin(origins = {"http://localhost:5173", "http://localhost:3000"})
 public class ProductController {
     @Autowired
     private ProductRepository productRepository;
