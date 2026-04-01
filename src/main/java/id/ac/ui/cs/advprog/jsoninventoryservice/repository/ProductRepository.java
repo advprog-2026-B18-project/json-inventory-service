c58360ad5a470c258d5350178f76a07f1dc8a54a
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-01 23:34:12 +0700

    refactor: add jpa specification executor

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/repository/ProductRepository.java
@@ -2,11 +2,12 @@ package id.ac.ui.cs.advprog.jsoninventoryservice.repository;
 
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product, UUID> {
+public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 
 }
