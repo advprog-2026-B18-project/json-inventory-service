a0bc584565b4dcde09a18e9568336a0b80d1cf66
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-12 01:16:38 +0700

    feat: implement pessimistic locking

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/repository/ProductRepository.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/repository/ProductRepository.java
@@ -1,13 +1,21 @@
 package id.ac.ui.cs.advprog.jsoninventoryservice.repository;
 
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.Product;
+import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Lock;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, UUID>, JpaSpecificationExecutor<Product> {
 
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    @Query("SELECT p FROM Product p WHERE p.id = :id")
+    Optional<Product> findByIdForUpdate(@Param("id") UUID id);
 }
