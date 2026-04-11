b480e4d448e9d5edad1d8de231a0fb8ff5c648f7
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-12 00:52:42 +0700

    feat: enable app scheduling

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/JsonInventoryServiceApplication.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/JsonInventoryServiceApplication.java
@@ -2,7 +2,9 @@ package id.ac.ui.cs.advprog.jsoninventoryservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class JsonInventoryServiceApplication {
 
