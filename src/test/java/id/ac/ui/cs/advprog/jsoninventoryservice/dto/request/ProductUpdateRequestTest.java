4a909f3eecffa1090ff60bb93f3c4b87e0045abb
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-16 02:45:32 +0700

    style: readability

--- src/test/java/id/ac/ui/cs/advprog/jsoninventoryservice/dto/request/ProductUpdateRequestTest.java
+++ src/test/java/id/ac/ui/cs/advprog/jsoninventoryservice/dto/request/ProductUpdateRequestTest.java
@@ -4,14 +4,13 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class ProductUpdateRequestTest {
-
     @Test
     void testProductUpdateRequest() {
         ProductUpdateRequest req = new ProductUpdateRequest();
-        req.setName("Tas Baru");
+        req.setName("New Bag");
         req.setStatus("OUT_OF_STOCK");
 
-        assertEquals("Tas Baru", req.getName());
+        assertEquals("New Bag", req.getName());
         assertEquals("OUT_OF_STOCK", req.getStatus());
     }
 }
