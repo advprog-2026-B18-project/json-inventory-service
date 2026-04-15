7acf6d2602a9f3c5f6012a8c3913a86864e9312c
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-16 02:12:58 +0700

    feat: add AuthIntegrationService get profile

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/service/AuthIntegrationService.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/service/AuthIntegrationService.java
@@ -1,7 +1,9 @@
 package id.ac.ui.cs.advprog.jsoninventoryservice.service;
 
+import java.util.Map;
 import java.util.UUID;
 
 public interface AuthIntegrationService {
     UUID getJastiperIdByUsername(String username);
+    Map<String, Object> getJastiperProfile(UUID userId);
 }
