74bf15269f6a1943150cda5edfc5480839f82982
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-16 02:25:21 +0700

    refactor: update StockManagementService interface

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/service/StockManagementService.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/service/StockManagementService.java
@@ -4,13 +4,13 @@ import id.ac.ui.cs.advprog.jsoninventoryservice.dto.request.PostOrderRequest;
 import id.ac.ui.cs.advprog.jsoninventoryservice.dto.request.StockReleaseRequest;
 import id.ac.ui.cs.advprog.jsoninventoryservice.dto.request.StockReserveRequest;
 import id.ac.ui.cs.advprog.jsoninventoryservice.dto.response.ProductResponse;
+import id.ac.ui.cs.advprog.jsoninventoryservice.dto.response.StockOperationResponse;
 
 import java.util.Optional;
 import java.util.UUID;
 
 public interface StockManagementService {
-    Optional<ProductResponse> reserveStock(UUID id, StockReserveRequest request);
-    Optional<ProductResponse> releaseStock(UUID id, StockReleaseRequest request);
+    Optional<StockOperationResponse> reserveStock(UUID id, StockReserveRequest request); Optional<ProductResponse> releaseStock(UUID id, StockReleaseRequest request);
     void cleanupExpiredReservations();
     Optional<ProductResponse> processPostOrder(UUID id, PostOrderRequest request);
 }
