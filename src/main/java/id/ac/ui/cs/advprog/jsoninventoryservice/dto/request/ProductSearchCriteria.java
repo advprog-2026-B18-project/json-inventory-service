6bc83b89af14766d5a62bf484279ef2cf377d7cf
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-21 14:16:31 +0700

    fix: add @JsonProperty to DTO

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/dto/request/ProductSearchCriteria.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/dto/request/ProductSearchCriteria.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.jsoninventoryservice.dto.request;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.enums.ProductStatus;
 import lombok.Builder;
 import lombok.Data;
@@ -10,12 +11,17 @@ import java.util.UUID;
 @Builder
 public class ProductSearchCriteria {
     private String keyword;
+    @JsonProperty("jastiper_id")
     private UUID jastiperId;
     private Long minPrice;
     private Long maxPrice;
+    @JsonProperty("category_id")
     private Integer categoryId;
     private ProductStatus status;
+    @JsonProperty("origin_country")
     private String originCountry;
+    @JsonProperty("date_from")
     private LocalDate dateFrom;
+    @JsonProperty("date_to")
     private LocalDate dateTo;
 }
