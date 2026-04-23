8426971b46048081b8dd9480a17f57403224ff56
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-04-23 23:24:27 +0700

    feat: add jpa auditing

---
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/config/JpaAuditingConfig.java
@@ -0,0 +1,9 @@
+package id.ac.ui.cs.advprog.jsoninventoryservice.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+
+@Configuration
+@EnableJpaAuditing
+public class JpaAuditingConfig {
+}
