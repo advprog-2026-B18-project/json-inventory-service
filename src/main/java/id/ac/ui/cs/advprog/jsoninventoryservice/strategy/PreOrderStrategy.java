ab3b117f359e6c2305c25e9d75556c0b0339917f
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-05-08 04:01:32 +0700

    refactor: throw pre-order exceptions

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/strategy/PreOrderStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/strategy/PreOrderStrategy.java
@@ -1,5 +1,6 @@
 package id.ac.ui.cs.advprog.jsoninventoryservice.strategy;
 
+import id.ac.ui.cs.advprog.jsoninventoryservice.exception.StockOperationException;
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.Product;
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.enums.ProductStatus;
 import org.springframework.stereotype.Component;
@@ -10,9 +11,17 @@ import java.time.LocalDate;
 public class PreOrderStrategy implements ShoppingModeStrategy {
     @Override
     public boolean isEligibleForReservation(Product product, int quantity) {
-        if (product.getPurchaseDate() != null && product.getPurchaseDate().isBefore(LocalDate.now())) {
-            return false;
+        if (product.getStatus() != ProductStatus.ACTIVE) {
+            throw new StockOperationException("This product is currently unavailable.", 400);
         }
-        return product.getStatus() == ProductStatus.ACTIVE && product.getStock() >= quantity;
+        if (product.getStock() < quantity) {
+            throw new StockOperationException("Insufficient quota available.", 400);
+        }
+
+        if (product.getPurchaseDate() != null && product.getPurchaseDate().isBefore(LocalDate.now())) {
+            throw new StockOperationException("Pre-order closed. Items are being purchased.", 400);
+        }
+
+        return true;
     }
 }
