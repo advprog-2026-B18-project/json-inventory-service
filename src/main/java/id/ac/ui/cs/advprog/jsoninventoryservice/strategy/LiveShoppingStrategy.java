b936c54fe6482f9a9cb8a0e86ed9914b43b8dc02
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-05-08 04:01:32 +0700

    refactor: throw live shopping exceptions

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/strategy/LiveShoppingStrategy.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/strategy/LiveShoppingStrategy.java
@@ -1,13 +1,22 @@
 package id.ac.ui.cs.advprog.jsoninventoryservice.strategy;
 
+import id.ac.ui.cs.advprog.jsoninventoryservice.exception.StockOperationException;
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.Product;
 import id.ac.ui.cs.advprog.jsoninventoryservice.model.enums.ProductStatus;
 import org.springframework.stereotype.Component;
 
 @Component
 public class LiveShoppingStrategy implements ShoppingModeStrategy {
+
     @Override
     public boolean isEligibleForReservation(Product product, int quantity) {
-        return product.getStatus() == ProductStatus.ACTIVE && product.getStock() >= quantity;
+        if (product.getStatus() != ProductStatus.ACTIVE) {
+            throw new StockOperationException("This product is currently unavailable.", 400);
+        }
+        if (product.getStock() < quantity) {
+            throw new StockOperationException("Insufficient stock.", 400);
+        }
+
+        return true;
     }
 }
