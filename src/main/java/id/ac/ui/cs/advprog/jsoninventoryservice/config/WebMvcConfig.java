8a5d6ee40d01cb139f7cb9b813916fdb00b4b621
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-05-19 15:36:20 +0700

    fix: resolve cors

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/config/WebMvcConfig.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/config/WebMvcConfig.java
@@ -1,13 +1,20 @@
 package id.ac.ui.cs.advprog.jsoninventoryservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 import java.util.List;
 
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
+
+    @Value("${app.frontend.url}")
+    private String[] frontendUrls;
+
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> argumentResolvers) {
         PageableHandlerMethodArgumentResolver resolver = new PageableHandlerMethodArgumentResolver();
@@ -15,5 +22,14 @@ public class WebMvcConfig implements WebMvcConfigurer {
         resolver.setSizeParameterName("limit");
         resolver.setOneIndexedParameters(true);
         argumentResolvers.add(resolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins(frontendUrls)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
 }
