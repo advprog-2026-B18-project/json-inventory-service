78767c3b2d744e2f99489c6f620f0beee28d994d
Raihana Auni Zakia <zakiaraihanauni@gmail.com>
2026-05-21 14:29:22 +0700

    fix: delete unused import

--- src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/service/S3StorageServiceImpl.java
+++ src/main/java/id/ac/ui/cs/advprog/jsoninventoryservice/service/S3StorageServiceImpl.java
@@ -2,7 +2,6 @@ package id.ac.ui.cs.advprog.jsoninventoryservice.service;
 
 import lombok.RequiredArgsConstructor;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
